ee060b66cb10b922842b6d890abc67562f9ec1b8
sandeepbiswal417-maker <sandeepbiswal417@gmail.com>
2025-11-19 08:28:43 +0530

    Add files via upload

---
+++ p4a (1).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int display(int a[],int n)
+{
+    int i=0;
+    for(i=0; i<=n;i++)
+        printf("%d\t",a[i]);
+    return 0;
+}
+int main()
+{
+    int arr[5]={10,20,30,40,50};
+    display (arr,5);
+return 0;
+}
