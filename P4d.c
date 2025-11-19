839ad8838ea893595c7a7b51f11cb78a9456599c
sandeepbiswal417-maker <sandeepbiswal417@gmail.com>
2025-11-19 09:08:28 +0530

    Update P4d.c

--- P4d.c
+++ P4d.c
@@ -1,26 +1,13 @@
-C Program for Nested if: positivity of two numbers
-
- #include <stdio.h>
-
-int main() {
-    int a, b;
-
-    printf("Enter two numbers: ");
-    scanf("%d %d", &a, &b);
-
-    if (a > 0) {  
-        if (b > 0) {  
-            printf("Both numbers are positive.\n");
-        } else {
-            printf("First number is positive, but second is not.\n");
-        }
-    } else {
-        if (b > 0) {
-            printf("First number is not positive, but second is.\n");
-        } else {
-            printf("Both numbers are not positive.\n");
-        }
-    }
-
-    return 0;
+#include<stdio.h>
+int* display(int a[],int index)
+{
+    return &a[index];
 }
+int main()
+{int arr[5]={50,10,40,30,20};
+int*p=display(arr,2);
+printf("%d",*p);
+return 0;
+}
+}
+
